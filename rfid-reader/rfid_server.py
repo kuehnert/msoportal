fa87e2a4968054acf7015eae446dedec32f43e35
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2019-03-15 15:10:36 +0100

    Fix missing literals

--- rfid-reader/rfid_server.py
+++ rfid-reader/rfid_server.py
@@ -6,19 +6,20 @@ import requests
 # import webbrowser
 
 reader = SimpleMFRC522.SimpleMFRC522()
-url = 'https://my-json-server.typicode.com/kuehnert/msoportal/students/'
+url = 'https://my-json-server.typicode.com/kuehnert/msoportal/students'
 
 try:
 	while True:
+		print("\n\nWaiting for scan")
 		id = reader.read()
 		print(f"Found student id: {id}")
 		print("Fetching data...")
-		response = requests.get(url=url)
+		response = requests.get(url=f"{url}/{id}")
 		print("Parsing data...")
 		data = response.json()
 		print("Found data...")
 		print(data)
-		# webbrowser.open_new_tab(url + id)
+		# webbrowser.open_new_tab(f"{url}/{id}")
 
 finally:
     GPIO.cleanup()
